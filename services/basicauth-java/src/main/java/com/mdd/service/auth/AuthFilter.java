1bd896eef6dccc74e3274679fdb41f66ca4e3e29
Dave Truxall <david.truxall@hp.com>
2015-01-06 21:44:07 -0500

    Functional HMAC service

--- services/basicauth-java/src/main/java/com/mdd/service/auth/AuthFilter.java
+++ services/basicauth-java/src/main/java/com/mdd/service/auth/AuthFilter.java
@@ -1,68 +1,61 @@
 package com.mdd.service.auth;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import java.security.SignatureException;
 import java.util.logging.Logger;
 
 
 public class AuthFilter implements ContainerRequestFilter {
     private static final Logger log = Logger.getLogger(AuthFilter.class.getName());
 
+    private static final String HMAC_PRIVATE = "4c4a3f0d-3dff-475a-afcc-6ec86fc0b126";
+
+    //This should really be from a user store, but for a demo it's just a constant
+    private static final String HMAC_USER = "CodeMash";
+
     @Override
     public ContainerRequest filter(ContainerRequest containerRequest) {
 
         //Get the HTTP authorization header
-        String auth = containerRequest.getHeaderValue("authorization");
+        String authHeader = containerRequest.getHeaderValue("authorization");
 
-        log.warning(String.format("Header: %s", auth));
+        log.warning(String.format("Header: %s", authHeader));
 
-        MultivaluedMap<String, String> querystring = containerRequest.getQueryParameters();
-        //If the user does not provide any credentials
-        if(auth == null && querystring.size() == 0){
+        if(authHeader == null || !authHeader.startsWith("MDD")){
             log.severe("No authentication info found");
             throw new WebApplicationException(Response.Status.UNAUTHORIZED);
         }
 
         boolean authenticated = false;
 
-        if(auth != null && auth.startsWith("Basic")) {
+        String[] creds = authHeader.replaceFirst("MDD ", "").split(":", 2);
 
-            String[] creds = BasicAuth.decode(auth);
+        //If login or password fail
+        if(creds == null || creds.length != 2){
+            log.severe("Invalid auth header - missing id or signature");
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
 
-            //If login or password fail
-            if(creds == null || creds.length != 2){
-                log.severe("Invalid basic auth credentials - missing id or password");
-                throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-            }
+        if(!creds[0].equalsIgnoreCase(HMAC_USER)){
+            log.severe("Invalid user");
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
 
-            //Really should check from an actual user store here
-            if("mdd".equalsIgnoreCase(creds[0]) && creds[1].equals("password123")) {
-                authenticated = true;
-            }
-        } else if(!querystring.isEmpty()){
+        String url = containerRequest.getRequestUri().toString();
 
-            if(querystring.containsKey("token")) {
-                String token = querystring.getFirst("token");
+        try {
+            String calcSignature = HmacAuth.calculateRFC2104HMAC(url, HMAC_PRIVATE);
 
-                Key clientKey = new Key();
+            authenticated = creds[1].equals(calcSignature);
 
-                Checker validator = new Checker(new String[]{clientKey.getGoogleKey()}, clientKey.getAudience());
-
-                GoogleIdToken.Payload payload = validator.check(token);
-
-                if(payload == null) {
-                    String error = validator.getProblem();
-                    log.severe(error);
-                    authenticated = false;
-                } else {
-                    authenticated = true;
-                }
-            }
+        } catch (SignatureException e) {
+            log.severe("Invalid signature calculation");
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
         }
 
         if(authenticated) {
