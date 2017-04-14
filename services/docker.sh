6f80ac912aa66cda6fe9c259e26253d5ca26a28c
David Truxall <david.truxall@hpe.com>
2017-04-14 16:40:58 -0400

    Set up for deployment via Docker

---
+++ services/docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+imageName="gdg-demo"
+
+#Create the new image
+docker build -t "${imageName}" .
+
+# Remove running instances
+running=$(docker ps -a -q -f 'name=${imageName}')
+
+for i in $running;
+do
+        echo "Stopping Container"
+        docker stop $i
+        echo "Removing Container"
+        docker rm $i
+done
+
+#Start a container with this image
+docker run --name gdg-api --restart=always -p 8000:8080 -d "${imageName}"
