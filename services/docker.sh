28f154deff5abbc0cfa568678761b8db9074ede6
David Truxall <david.truxall@hpe.com>
2017-04-14 17:06:59 -0400

    Fixing namimg problems

--- services/docker.sh
+++ services/docker.sh
@@ -6,7 +6,7 @@ imageName="gdg-demo"
 docker build -t "${imageName}" .
 
 # Remove running instances
-running=$(docker ps -a -q -f 'name=${imageName}')
+running=$(docker ps -a -q -f "name=${imageName}")
 
 for i in $running;
 do
@@ -17,4 +17,4 @@ do
 done
 
 #Start a container with this image
-docker run --name gdg-api --restart=always -p 8003:8080 -d "${imageName}"
+docker run --name "${imageName}" --restart=always -p 8003:8080 -d "${imageName}"
