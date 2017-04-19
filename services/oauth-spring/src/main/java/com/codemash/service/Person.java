58a830ae60d45dd2d18bd8240ffb3a59ddc31cf2
David Truxall <david.truxall@hpe.com>
2017-04-19 14:41:14 -0400

    Added oAuth project

---
+++ services/oauth-spring/src/main/java/com/codemash/service/Person.java
@@ -0,0 +1,38 @@
+package com.codemash.service;
+
+/**
+ * Created by david on 12/22/14.
+ */
+
+import java.io.Serializable;
+
+
+public class Person implements Serializable {
+    private String FirstName;
+    private String LastName;
+    private String Gender;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String _FirstName) {
+        this.FirstName = _FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+}
